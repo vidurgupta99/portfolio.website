98e3bebc88b39cb02b72740a63fcc3d44474b260
vidurgupta2 <70779106+vidurgupta2@users.noreply.github.com>
2023-03-01 20:00:29 -0500

    Final Updates

--- links/app.js
+++ links/app.js
@@ -2,43 +2,35 @@ $(() => {
 
     // Elements Created for JQuery
     
-    $linkedinDiv = $('#linkedinParagraph') // 'linkedinParagraph' '#bioparagraph'
-    $githubDiv = $('#github-paragraph') // 'resume-paragraph'
-    $extraDiv = $('#projects-divy')
+    $linkedinDiv = $('#linkedinParagraph')
+    $githubDiv = $('#github-paragraph')
+    $extraDiv = $('#extraDiv')
 
-    
-    
-    
     // On Load, show/hide:
     $linkedinDiv.hide()
     $extraDiv.hide()
     $githubDiv.hide()
-      
-  
-    
+          
     // Funtions for on-click:
+
+    const closeAndExpand = () => {
+        $extraDiv.slideToggle(700)
+        $linkedinDiv.slideUp(700)
+        $githubDiv.slideUp(700)
+    }
     const expandContainer = () => {
         $linkedinDiv.slideToggle(700)
         $githubDiv.slideUp(700)
         $extraDiv.slideUp(700)
     }
     
-    const closeAndExpand = () => {
-        $extraDiv.slideToggle(700)
-        $linkedinDiv.slideUp(700)
-        $githubDiv.slideUp(700)
-    }
     const expandOther = () => {
         $githubDiv.slideToggle(700)
         $linkedinDiv.slideUp(700)
         $extraDiv.slideUp(700)
     }
    
-    // OnClick Actions
-        $('#github').on('click', expandContainer) // #about-header. github-header
-        $('#extra').on('click', closeAndExpand) // 'extra'
+        $('#github').on('click', expandContainer)
+        $('#extra').on('click', closeAndExpand)
         $('#linkedin').on('click', expandOther)
-     
-    
-    
     })
